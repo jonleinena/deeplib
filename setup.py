d8b7bd057cf2cdb9cbcc61ca7c7a1a327ffe1998
jleinena <jleinena@vicomtech.org>
2024-12-16 14:27:40 +0100

    added necessary files to create pypi package

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
 setup(
     name="deeplib",
-    version="0.1",
+    version="0.1.0",
+    author="Jon Leiñena",
+    author_email="leinenajon@gmail.com",
+    description="A deep learning library for computer vision tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonleinena/deeplib",
     packages=find_packages(),
-    install_requires=[
-        "torch",
-        "torchvision",
-        "albumentations",
-        "opencv-python",
-        "numpy",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
+    python_requires=">=3.10",
+    install_requires=requirements,
 ) 
